41d90c8f2d5a49174ea481d0452de4b89ffcde33
JeffGarland <jeff@crystalclearsoftware.com>
2014-09-12 06:49:17 -0700

    initial roundtrip tests for day_of_year

--- libs/date_time/test/v2/test_day_of_year.hpp
+++ libs/date_time/test/v2/test_day_of_year.hpp
@@ -6,7 +6,6 @@
 
 
 #include "boost/date_time/v2/day_of_year.hpp"
-#include <iostream>
 #include "testfrmwk.hpp"
 
 void test_day_of_year() 
@@ -19,28 +18,49 @@ void test_day_of_year()
   //Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
   // 31  59  90 120 151 181 212 243 273 304 334 365 
 
-  {
+  { //january
+
     day_of_year doy1(2014, 1);
     date d1(doy1);
     check("day of year - Jan 1", date(2014, Jan, 1) == d1);
+
+    day_of_year doy1a = d1.to<day_of_year>();
+    check("day of year - Jan 1 - roundtrip",  
+	  doy1a.year == 2014 && doy1a.day == 1);
 
     day_of_year doy2(2014, 31);
     date d2(doy2);
     check("day of year - Jan 31", date(2014, Jan, 31) == d2);
 
+    day_of_year doy2a = d2.to<day_of_year>();
+    check("day of year - Jan 31 - roundtrip",  
+	  doy2a.year == 2014 && doy2a.day == 31);
+
     day_of_year doy3(2000, 31);
     date d3(doy3);
     check("day of year - Jan 31 - leap", date(2000, Jan, 31) == d3);
 
+    day_of_year doy3a = d3.to<day_of_year>();
+    check("day of year - Jan 31 - roundtrip - leap",  
+	  doy3a.year == 2000 && doy3a.day == 31);
+
   }
-  {
+  { //feb
     day_of_year doy1(2014, 31+1);
     date d1(doy1);
     check("day of year - Feb 1", date(2014, Feb, 1) == d1);
 
+    day_of_year doy1a = d1.to<day_of_year>();
+    check("day of year - Feb 1 - roundtrip",  
+	  doy1a.year == 2014 && doy1a.day == 32);
+
     day_of_year doy2(2014,31+28);
     date d2(doy2);
     check("day of year - Feb 28 - no leap", date(2014, Feb, 28) == d2);
+
+    day_of_year doy2a = d2.to<day_of_year>();
+    check("day of year - Feb 28 - no leap - roundtrip",  
+	  doy2a.year == 2014 && doy2a.day == 31+28);
 
     day_of_year doy3(2000, 31+29);
     date d3(doy3);
@@ -63,7 +83,6 @@ void test_day_of_year()
     day_of_year doy3(2000, 31+29+31);
     date d3(doy3);
     check("day of year - Mar 31 - leap", date(2000, Mar, 31) == d3);
-
 
   }
 
