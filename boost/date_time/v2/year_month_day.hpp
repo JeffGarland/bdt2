77b8fdd4e94c13187d5381e2b4c0240f1b3667d4
JeffGarland <jeff@crystalclearsoftware.com>
2015-05-26 18:03:31 -0600

    various cleanups - constexpr additions

--- boost/date_time/v2/year_month_day.hpp
+++ boost/date_time/v2/year_month_day.hpp
@@ -6,10 +6,11 @@
 #include <string>
 #include <tuple>
 #include <array>
-#include <boost/date_time.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/finder.hpp>
 #include "basic_defs.hpp"
+#include <iomanip>
 
 namespace algo = boost::algorithm;
 
@@ -22,6 +23,7 @@ namespace boost {
     struct year_month_day 
     {
       ///Constructs and invalid ymd
+      constexpr
       year_month_day() noexcept :
 	year(0),
 	month(0),
@@ -29,6 +31,7 @@ namespace boost {
       {}
 
       ///Construct a year month day -- unchecked
+      constexpr
       year_month_day(int16_t y, uint8_t m, uint8_t d) noexcept :
 	year(y),
 	month(m),
@@ -45,34 +48,65 @@ namespace boost {
       // }
 
       //todo -- handy, but dangerous...what if month is actually a string...
-      year_month_day(const char* const y, const char* const m, const char* const d) :
+      year_month_day(const char* const y,
+		     const char* const m,
+		     const char* const d) :
 	year(std::atoi(y)),
 	month(std::atoi(m)),
 	day_of_month(std::atoi(d))
       {}
 
       template<typename T>
+      explicit
+      constexpr
       year_month_day(const T& y, const T& m, const T& d) :
 	year(static_cast<int16_t>(y)),
 	month(static_cast<uint8_t>(m)),
 	day_of_month(static_cast<uint8_t>(d)) 
       {} //todo should cause error
 
-      // todo -- shouldn't we have an orderinging and delimiter version
-      /// Construct from a string of the form YYYY-MM-DD or YYYY/MM/DD
-      year_month_day(const char* const ymd_string) 
+      enum class date_order { ymd, mdy, dmy };
+
+
+      template<date_order=date_order::ymd>
+      static
+      year_month_day
+      parse_date(const char* const ymd_string, date_order)
       {
 	std::string ymd(ymd_string);
 	algo::replace_all(ymd, "/", " ");
 	algo::replace_all(ymd, "-", " ");
-	year = std::atoi(ymd.c_str());
-	month = std::atoi(ymd.c_str()+5);
-	day_of_month = std::atoi(ymd.c_str()+8);
-	//todo remove	std::cout << "ymd char*: " << ymd << std::endl;
+	return year_month_day(std::atoi(ymd.c_str()),
+			      std::atoi(ymd.c_str()+5),
+			      std::atoi(ymd.c_str()+8));
+      }
+
+      // template<>
+      // static
+      // year_month_day
+      // parse_date<date_order::mdy>(const char* const ymd_string, date_order) :
+      // {
+      // 	std::string ymd(ymd_string);
+      // 	algo::replace_all(ymd, "/", " ");
+      // 	algo::replace_all(ymd, "-", " ");
+      // 	return year_month_day(std::atoi(ymd.c_str()),
+      // 			      std::atoi(ymd.c_str()+5),
+      // 			      std::atoi(ymd.c_str()+8));
+
+      // }
+
+      // todo -- shouldn't we have an ordering and delimiter version
+      /// Construct from a string of the form YYYY-MM-DD or YYYY/MM/DD
+      template<date_order = date_order::ymd>
+      year_month_day(const char* const ymd_string, 
+		     date_order order=date_order::ymd) 
+      {
+	*this = parse_date(ymd_string, order);
       }
 
       template<typename T>
-      explicit year_month_day(const T& ymd)
+      explicit 
+      year_month_day(const T& ymd)
       {
       } //todo should cause error
 
@@ -83,40 +117,46 @@ namespace boost {
 	//    static_assert(false, "This signature is here for speciazialition only");
       }
 
-
-      // template<typename T>
-      // year_month_day(T ymd)
-      // {} //todo should cause error
       bool operator==(const year_month_day& rhs) const
       {
-	return (year == rhs.year && month == rhs.month && day_of_month == rhs.day_of_month);
+	return (year == rhs.year &&
+		month == rhs.month &&
+		day_of_month == rhs.day_of_month);
       }
-      int16_t year;// = 10001; //todo - what the zero year -- what the negative?
-      uint8_t  month;// = 13;
-      uint8_t  day_of_month;//=32;
+      int16_t year;//   = 10001; //todo - what the zero year -- what the negative?
+      uint8_t month;//  = 13;
+      uint8_t day_of_month; // = 32;
     };
 
 
+    ///Stream out the year/month/day
     std::ostream& operator<<(std::ostream& os, const year_month_day& ymd)
     {
       os << ymd.year << "-" 
-	 << std::setw(2) << std::setfill('0') << static_cast<unsigned short>(ymd.month) << "-" 
-	 << std::setw(2) << std::setfill('0') << static_cast<unsigned short>(ymd.day_of_month);
+	 << std::setw(2) << std::setfill('0') 
+	 << static_cast<unsigned short>(ymd.month) << "-" 
+	 << std::setw(2) << std::setfill('0') 
+	 << static_cast<unsigned short>(ymd.day_of_month);
       return os;
     }
 
+    ///Specialization to convert from 3 strings in y, m, d order
     template<>
-    year_month_day::year_month_day(const std::string& y, const std::string& m, const std::string& d) :
+    year_month_day::year_month_day(const std::string& y,
+				   const std::string& m,
+				   const std::string& d) :
       year(std::atoi(y.c_str())),
       month(std::atoi(m.c_str())),
       day_of_month(std::atoi(d.c_str()))
     {}
 
+    ///Specialization to parse a string - forwards to char* specialization
     template<>
     year_month_day::year_month_day(const std::string& ymd_string) :
       year_month_day(ymd_string.c_str())
     {}
 
+    ///Specialization to convert from std::tm
     template<>
     year_month_day::year_month_day(const std::tm& tm)
     {
@@ -125,6 +165,7 @@ namespace boost {
       day_of_month = tm.tm_mday;
     } 
 
+    ///Specialization to convert from a std::tuple<int,int,int> in y/m/d order
     template<>
     year_month_day::year_month_day(const std::tuple<int, int, int>& ymd)
     {
